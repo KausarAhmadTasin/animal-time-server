64543bcbc147ead463c049a6cbe1e8c88fdf9570
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2024-09-20 16:23:01 +0600

    category post and get api added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -9,7 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dqs9o84.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,14 +24,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const categoryCollection = client
       .db("animal_time")
       .collection("categoryCollection");
 
-    app.post("/category", async (req, res) => {
-      const category = req.body;
+    app.get("/categories", async (req, res) => {
+      const categories = await categoryCollection.find().toArray();
+      res.send(categories);
+    });
+
+    app.post("/categories", async (req, res) => {
+      let categoryName = req.body.category_name;
+
+      categoryName =
+        categoryName.charAt(0).toUpperCase() +
+        categoryName.slice(1).toLowerCase();
+
+      const category = { ...req.body, category_name: categoryName };
+
+      const query = { category_name: new RegExp(`^${categoryName}$`, "i") };
+
+      const existingCategory = await categoryCollection.findOne(query);
+
+      if (existingCategory) {
+        return res.status(400).send({ message: "Category already exists" });
+      }
 
       const result = await categoryCollection.insertOne(category);
       res.send(result);
@@ -44,7 +63,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
